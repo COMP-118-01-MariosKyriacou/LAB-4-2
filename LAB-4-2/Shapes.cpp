6e36687425be23f5533c668e66f477748af00968
Marios Kyriacou <kyriacou.m6@live.unic.ac.cy>
2025-05-17 21:49:29 +0300

    Implemented the function of drawSquare(). Gets a parameter of size(size of square), character and filled. If filled is true it means that it's been called as a filled function and it draws a filled square otherwise it asks the user if they want it to be filled or not

--- LAB-4-2/Shapes.cpp
+++ LAB-4-2/Shapes.cpp
@@ -44,3 +44,48 @@ void drawVerticalLine(const int height, const char ch) {
     
     cout << endl;
 }
+
+/**
+  * Create a square with the desired size. The boolean filled is so it checks if it wants it filled or not. The boolean will be <code>false</code> if it wasn't called by the user, hence the shape will not be filled. If the function gets called from the random shape generator then it gets the default state of not-filled.
+  * @param size The size variable is both the width and height of the square
+  * @param ch The ch variable is the desired character that they want the shape to have
+  * @param filled Checks if the function has been called from the random number generator or from the user
+ */
+void drawSquare(const int size, const char ch, const bool filled){
+    assert(size > 0);
+    
+    char tempAns;
+    
+    if(!filled) {
+        // Prompt user to ask them if they want the square filled or empty
+        cout << "Would you like a filled square (y/n)? ";
+        cin >> tempAns;
+        
+        // Data validation for yes and no
+        while(tempAns != 'y' && tempAns != 'n') {
+            cout << tempAns;
+            cout << "Please enter y or n: ";
+            cin >> tempAns;
+        }
+    }
+    
+    // If the answer is y then draw a filled square otherwise draw an empty square
+    if(tempAns == 'y') drawSquareFilled(size, ch);
+    else {
+        // Loop through the size. If it's the first row, draw the whole row. Any other row, draw a character add a space of
+        // size - 1 (setw starts from 1, so we remove 1 from size) and add another character
+        for(int i = 0; i < size; i++) {
+            if(i == 0 || i == size - 1) {
+                for(int j = 0; j < size; j++) {
+                    cout << ch;
+                }
+                cout << endl;
+            } else {
+                cout << ch << setw(size - 1) << ch;
+                cout << endl;
+            }
+        }
+        
+        cout << endl;
+    }
+}
