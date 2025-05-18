5232d1e87425ea44faaa161242f39fa315297b01
Marios Kyriacou <kyriacou.m6@live.unic.ac.cy>
2025-05-18 10:40:16 +0300

    Initial Commit

---
+++ LAB-4-2-2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LAB-4-2-2
+//
+//  Created by Marios Kyriacou on 18/05/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
