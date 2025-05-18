d54576a720fba565c2f36705dd247f466684be04
Marios Kyriacou <kyriacou.m6@live.unic.ac.cy>
2025-05-18 10:52:09 +0300

    Added all of the function declarations inside of Matrix.h

--- LAB-4-2-2/Matrix.h
+++ LAB-4-2-2/Matrix.h
@@ -7,3 +7,18 @@
  * \version 0.1
  * \copyright GNU Public License
  */
+
+#include "namespaces.h"
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+double sumOfRow(const double[][Globals::MAX_COL], const int, const int);
+double sumOfCol(const double[][Globals::MAX_COL], const int, const int);
+void fillWithRandomNum(double[][Globals::MAX_COL], const int);
+void printMatrix(const double [][Globals::MAX_COL], const int);
+double maximumValue(const double[][Globals::MAX_COL], const int);
+void transposedMatrix(const double[][Globals::MAX_COL], double[][Globals::MAX_COL], const int);
+double averageOfElements(const double[][Globals::MAX_COL], const int);
+
+#endif
